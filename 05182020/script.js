6473d3747b1e49c380aeae0d150b6adb034bd61b
twilson123 <64107591+twilson123@users.noreply.github.com>
2021-01-29 15:04:43 -0500

    Add files via upload

---
+++ 05182020/script.js
@@ -0,0 +1,64 @@
+alert(jQuery);
+
+
+
+
+
+
+
+
+
+
+/*const getData  =() => {
+  axios.get('http://csc225.mockable.io/books').then(response => {
+    console.log(response);
+  });
+};
+
+const sendData = () => {
+  axios.post();
+};*/
+
+
+
+/*{
+    "books": [
+    {
+      "id": 1,
+      "author": "Chinua Achebe",
+      "title": "Things Fall Apart"
+      
+    },
+    {
+      "id": 2,
+      "author": "Hans Christian Andersen",
+      "title": "Fairy tales"
+      
+    },
+    {
+      "id": 3,
+      "author": "Dante Alighieri",
+      "title": "The Divine Comedy"
+    },
+    {
+      "id": 4,
+      "author": "Homer",
+      "title": "Iliad"
+    },
+    {
+      "id": 5,
+      "author": "Herman Melville",
+      "title": "Moby Dick"
+    },
+    {
+        "id": 6,
+        "author": "JD Salinger",
+        "title": "Catcher in the Rye"
+    },
+      {
+        "id": 7,
+        "author": "Albert Camus",
+        "title": "L'Étranger (The Stranger)"
+    }
+  ]
+}*/
